e4e4a0bffca69ab8d57721cf129643d4b2c1943c
IOxxy <gionatan99@gmail.com>
2018-03-14 22:27:55 +0100

    Update server and package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var bodyParser = require('body-parser');
 
 var tools = require('./lib/tools');
